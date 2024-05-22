be8845bcc33ed32221b79f5e26fdafbb373aa3c0
Tomas Connor <tomas.connor@mluvii.com>
2024-05-22 21:41:06 +0200

    add tailwind css and fix dependencies

--- frontend/src/components/CalculateIntake.js
+++ frontend/src/components/CalculateIntake.js
@@ -1,29 +1,7 @@
 import React, { useState } from 'react';
 import { Formik, Form, Field } from 'formik';
-import styled from 'styled-components';
 import { ClipLoader } from 'react-spinners';
 import axiosInstance from '../axiosConfig';
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 50px;
-`;
-
-const Button = styled.button`
-  padding: 10px 20px;
-  margin-top: 20px;
-  background-color: #4CAF50;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-`;
-
-const FieldContainer = styled.div`
-  margin-bottom: 10px;
-`;
 
 const CalculateIntake = () => {
   const [loading, setLoading] = useState(false);
@@ -34,13 +12,16 @@ const CalculateIntake = () => {
     weight_kg: '',
     age: '',
     gender: '',
-    activity_level: ''
+    activity_level: '',
   };
 
   const handleSubmit = async (values, { setSubmitting }) => {
     setLoading(true);
     try {
-      const response = await axiosInstance.post('/api/calculate-intake', values);
+      const response = await axiosInstance.post(
+        '/api/calculate-intake',
+        values
+      );
       setResult(response.data);
     } catch (error) {
       alert('Error');
@@ -50,53 +31,96 @@ const CalculateIntake = () => {
   };
 
   return (
-    <Container>
-      <h1>Calculate Intake</h1>
+    <div className="flex flex-col items-center mt-12">
+      <h1 className="text-xl font-bold text-center text-gray-800 mt-8 mb-4">
+        Calculate Intake
+      </h1>
+
       <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         {({ isSubmitting }) => (
-          <Form>
-            <FieldContainer>
-              <Field type="number" name="user_id" placeholder="User ID" />
-            </FieldContainer>
-            <FieldContainer>
-              <Field type="number" name="age" placeholder="Age" />
-            </FieldContainer>
-            <FieldContainer>
-              <Field type="number" name="height_cm" placeholder="Height (cm)" />
-            </FieldContainer>
-            <FieldContainer>
-              <Field type="number" name="weight_kg" placeholder="Weight (kg)" />
-            </FieldContainer>
-            <FieldContainer>
-              <Field as="select" name="gender">
-                <option value="" label="Select gender" />
-                <option value="male" label="Male" />
-                <option value="female" label="Female" />
-              </Field>
-            </FieldContainer>
-            <FieldContainer>
-              <Field as="select" name="activity_level">
-                <option value="" label="Select activity level" />
-                <option value="low" label="Low" />
-                <option value="medium" label="Medium" />
-                <option value="high" label="High" />
-              </Field>
-            </FieldContainer>
-            <Button type="submit" disabled={isSubmitting || loading}>Calculate</Button>
-            {loading && <ClipLoader size={50} color={"#4CAF50"} loading={loading} />}
+          <Form className="flex flex-col">
+            <Field
+              className="mb-5 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              type="number"
+              name="user_id"
+              placeholder="User ID"
+            />
+
+            <Field
+              className="mb-5 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              type="number"
+              name="age"
+              placeholder="Age"
+            />
+
+            <Field
+              className="mb-5 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              type="number"
+              name="height_cm"
+              placeholder="Height (cm)"
+            />
+
+            <Field
+              className="mb-5 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              type="number"
+              name="weight_kg"
+              placeholder="Weight (kg)"
+            />
+
+            <Field
+              className="mb-5 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              as="select"
+              name="gender"
+            >
+              <option value="" label="Select gender" />
+              <option value="male" label="Male" />
+              <option value="female" label="Female" />
+            </Field>
+
+            <Field
+              className="mb-5 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              as="select"
+              name="activity_level"
+            >
+              <option value="" label="Select activity level" />
+              <option value="low" label="Low" />
+              <option value="medium" label="Medium" />
+              <option value="high" label="High" />
+            </Field>
+
+            <button
+              type="submit"
+              disabled={isSubmitting || loading}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Calculate
+            </button>
+
+            {loading && (
+              <ClipLoader size={50} color={'#4CAF50'} loading={loading} />
+            )}
           </Form>
         )}
       </Formik>
+
       {result && (
-        <div>
+        <>
           <h2>Daily Intake</h2>
-          <p><strong>Calories:</strong> {result.calories}</p>
-          <p><strong>Protein (g):</strong> {result.protein_g}</p>
-          <p><strong>Fat (g):</strong> {result.fat_g}</p>
-          <p><strong>Sugar (g):</strong> {result.sugar_g}</p>
-        </div>
+          <p>
+            <strong>Calories:</strong> {result.calories}
+          </p>
+          <p>
+            <strong>Protein (g):</strong> {result.protein_g}
+          </p>
+          <p>
+            <strong>Fat (g):</strong> {result.fat_g}
+          </p>
+          <p>
+            <strong>Sugar (g):</strong> {result.sugar_g}
+          </p>
+        </>
       )}
-    </Container>
+    </div>
   );
 };
 
