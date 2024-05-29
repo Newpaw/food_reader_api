32b4b0f8c9bacef168437e69baab9bb84e817ba5
Jan Novopacký <jan.novopacky@gmail.com>
2024-05-29 16:02:55 +0200

    start adding UserMetrics

--- backend/src/models.py
+++ backend/src/models.py
@@ -4,6 +4,7 @@ from datetime import datetime, timezone
 from sqlalchemy.orm import relationship
 from .database import Base
 
+
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
@@ -12,9 +13,25 @@ class User(Base):
 
     calories = relationship("UserCalories", back_populates="owner")
     food_info = relationship("FoodInfoDB", back_populates="owner")
+    user_metrics = relationship("UserMetrics", back_populates="owner")
 
     def verify_password(self, password: str):
         return hash.bcrypt.verify(password.encode('utf-8'), self.hashed_password)
+
+
+class UserMetrics(Base):
+    __tablename__ = "user_metrics"
+    id = Column(Integer, primary_key=True, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    height_cm = Column(Float, nullable=False)
+    weight_kg = Column(Float, nullable=False)
+    age = Column(Float, nullable=False)
+    gender = Column(String, nullable=False)
+    activity_level = Column(String, nullable=False)
+    date_created = Column(DateTime, default=datetime.now(timezone.utc))
+    date_last_updated = Column(DateTime, default=datetime.now(timezone.utc))
+
+    owner = relationship("User", back_populates="metrics")
 
 
 class UserCalories(Base):
